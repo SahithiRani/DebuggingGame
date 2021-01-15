16195b0380f39c9ab62c32bf92010834fa667196
SahithiRani <sahithirani2002@gmail.com>
2021-01-15 09:21:03 +0530

    hp

---
+++ Monkey Game/Assets/shiv.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shiv : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+    private static shiv instance = null;
+    private static shiv Instance
+    {
+        get { return instance; }
+    }
+    void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+    // Update is called once per frame
+}
